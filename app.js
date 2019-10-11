d193b0227c8b76cac8a30bd5490581c0e0d60eae
muna alshorman <munaalshorman123@gmail.com>
2019-10-12 02:31:43 +0300

    solve problem6

--- app.js
+++ app.js
@@ -23,14 +23,9 @@ var resultString = 'The sum of ' +  a  + ' and ' + b + ' is ' + c +'.' ;
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the
- first element is the product of those numbers, and the second element is a string that EXACTLY follows
-  this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function: "The product of 5 and 9 is 45."
 
-"The product of 5 and 9 is 45."
-
-Test this function by hand in the console to get it working, and when you think it is finished,
- uncomment the call for the testMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
@@ -123,15 +118,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
   var multiplyOfArray = multiply(multiply(testArray[0],testArray[1])[0],testArray [2])[0];
-  console.log(testArray[0])
+  //console.log(testArray[0])
   var resultString ='The numbers '+ testArray[0]+','+ testArray[1]+','+ testArray[2] +' have a product of ' + multiplyOfArray +'.'
-  console.log(resultString);
+  //console.log(resultString);
   return [multiplyOfArray,resultString];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
- testMultiplyArray(testArray);
+// testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -152,15 +147,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 This function should be dynamic, accepting an array of any length.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
-
+// + multiplyOfArray +
 // Write your code here
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
-
+//testDynamicArray.push(1);
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+  var multiplyOfArray =1;
+  for(var  i = 0 ; i < testDynamicArray.length ; i++){
+    multiplyOfArray= multiply(multiplyOfArray,testDynamicArray[i])[0];
+  }
+  
+var resultString ='The numbers '+ testDynamicArray.toString() +' have a product of '+multiplyOfArray+'.';
+return [ multiplyOfArray,resultString];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
